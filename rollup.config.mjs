75397c52942b0be83972814f8af656857cdc2368
Teon L Brooks <teon.brooks@gmail.com>
2025-05-15 18:09:26 -0400

    wip: plugin config still raises error

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,27 @@
 import { makeRollupConfig } from "@jspsych/config/rollup";
+import esbuild from "rollup-plugin-esbuild";
 
-export default makeRollupConfig("jsPsychPluginVoiceResponse");
+const config = makeRollupConfig("jsPsychPluginVoiceResponse");
+
+const newConfig = config.map((config) => {
+  if (config.output.file?.endsWith(".browser.min.js")) {
+    const modifiedConfig = {
+      ...config,
+      plugins: [
+        ...config.plugins,
+        esbuild({
+          // loaders: "json",
+          minify: true,
+          target: "es2020",
+        }),
+      ],
+    };
+
+    return modifiedConfig;
+  } else {
+    return config;
+  }
+});
+
+console.log(newConfig);
+export default newConfig;
